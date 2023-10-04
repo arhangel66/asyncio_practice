18ca3aa9f81fc2ba5cf66affbfce0a17a296684a
Mikhail Derbichev <arhangel662@gmail.com>
2023-10-04 06:26:01 +0400

    "Enhanced event_loop.py with server and client handling functionality."

--- event_loop.py
+++ event_loop.py
@@ -1,31 +1,75 @@
 import asyncio
+import datetime
+import logging
 import signal
+import sys
+from asyncio import StreamReader, StreamWriter
+from time import sleep
 
-# Функция для обработки сигналов
-def signal_handler(task, signal):
-    print(f'Received signal {signal}, cancelling the task...')
-    task.cancel()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+logger.addHandler(logging.StreamHandler(stream=sys.stdout))
 
-# Асинхронная функция main
-async def main():
-    print('Event loop running...')
+def display_datetime():
+    print(f'Current datetime: {datetime.datetime.now()}')
+
+async def signal_handler():
+    print('Received signal, cancelling tasks...')
+    for task in asyncio.all_tasks():
+        task.cancel()
+
+clients = []
+
+async def handle_client(reader, writer):
+    clients.append(writer)
     try:
         while True:
-            await asyncio.sleep(1)
-            print('Still running...')
+            data = await reader.read(100)
+            if not data:
+                break
+    finally:
+        clients.remove(writer)
+        writer.close()
+        await writer.wait_closed()
+
+async def send_status():
+    while True:
+        await asyncio.sleep(2)
+        now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        message = f'Current time: {now}, connected clients: {len(clients)}\n'
+        for client in clients:
+            client.write(message.encode())
+            await client.drain()
+
+async def create_server():
+    server = await asyncio.start_server(handle_client, '127.0.0.1', 8088)
+    async with server:
+        send_status_task = asyncio.create_task(send_status())
+        try:
+            await asyncio.Event().wait()  # Keep the server running
+        finally:
+            send_status_task.cancel()
+
+async def main():
+    print('Event loop running...')
+    loop.call_later(3, display_datetime)
+
+    try:
+        await create_server()
     except asyncio.CancelledError:
-        print('Task has been cancelled')
+        print("Main task was cancelled")
+
+
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
-    task = loop.create_task(main())
 
     # Добавление обработчиков сигналов
-    loop.add_signal_handler(signal.SIGINT, signal_handler, task, 'SIGINT')
-    loop.add_signal_handler(signal.SIGTERM, signal_handler, task, 'SIGTERM')
+    loop.add_signal_handler(signal.SIGINT, lambda: loop.create_task(signal_handler()))
+    loop.add_signal_handler(signal.SIGTERM, lambda: loop.create_task(signal_handler()))
 
     try:
-        loop.run_until_complete(task)
+        loop.run_until_complete(main())
     finally:
         print('Cleaning up and shutting down...')
         loop.close()
